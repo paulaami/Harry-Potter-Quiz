26d18a8e900f9d5d1e44b84ae1fe3ef6f47a026e
paulaami <pmilkowska@onet.pl>
2021-06-27 12:43:39 +0200

    changed index.html

--- js/end.js
+++ js/end.js
@@ -26,5 +26,5 @@ saveHighScore = e => {
  highScores.splice(5)
 
  localStorage.setItem('highScores', JSON.stringify(highScores))
- window.location.assign('/start.html')
+ window.location.assign('/index.html')
 }
